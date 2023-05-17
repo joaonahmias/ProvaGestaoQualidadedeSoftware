92f653436d80f0595897c6277e3a098c90845790
joaonahmias <joaovictornahmias@gmail.com>
2023-05-16 21:23:19 -0300

    Update pré-prova

--- tests/queries_turma.py
+++ tests/queries_turma.py
@@ -1,8 +1,5 @@
 from ConexaoDB import *
 
-def alunos_de_uma_turma(bd):
-    return ler_bd(bd, "SELECT a.nome FROM media_aluno_turma m, turma t, aluno a WHERE t.codigo = 'TAD0203' ")
-
 def medias_acima_de_9(bd):
     return ler_bd(bd,"SELECT a.nome FROM media_aluno_turma m, aluno a WHERE media >=9 AND  m.id_aluno = a.id ")
 
@@ -10,9 +7,23 @@ def turmas_de_um_aluno(bd,aluno):
    query = "SELECT t.nome FROM media_aluno_turma m, aluno a, turma t WHERE a.nome = ? AND m.id_aluno = a.id AND m.id_turma = t.id "
    return ler_bd(bd, query,(aluno,))
 
-def alunos_de_uma_turma(bd):
-   query = "SELECT a.nome FROM media_aluno_turma m, aluno a, turma t WHERE t.codigo = 'TAD0203' AND m.id_aluno = a.id AND m.id_turma= t.id  "
-   return ler_bd(bd, query)
+def alunos_de_uma_turma(bd,turma):
+   query = "SELECT a.nome FROM media_aluno_turma m, aluno a, turma t WHERE t.codigo = ? AND m.id_aluno = a.id AND m.id_turma= t.id  "
+   return ler_bd(bd, query,(turma,))
+
+def quantidade_de_alunos(bd,turma):
+   query = "SELECT COUNT(m.id_turma) FROM media_aluno_turma m, turma t WHERE t.nome=? AND m.id_turma=t.id"
+   return ler_bd(bd, query,(turma,))
+
+#aluno por id
+#aluno por nome
+#media por aluno
+#selecionar medias dos alunos de uma turma
+#selecionar alunos, turmas e suas medias
+#alunos e suas turmas
+#selecionar todas as medias dos alunos de uma turma
+#notas acima de 7
+#todas as medias dos alunos nas turma
 
 
 
--- tests/test_turma.py
+++ tests/test_turma.py
@@ -5,19 +5,35 @@ from ConexaoDB import *
 from queries_turma import *
 
 class Test_turma(MockBD):
-    #def test_alunos_de_uma_turma(self):
-        #r_esperado = [('Carla',), ('Lara',), ('Liryel',)]
-        #self.assertEqual(alunos_de_uma_turma(self.mock_db_config.get('bd')),r_esperado)
 
     def test_medias_acima_de_9(self):
         r_esperado = [('Carla',), ('Lara',), ('Liryel',)]
         self.assertEqual(medias_acima_de_9(self.mock_db_config.get('bd')),r_esperado)
     
     def test_turmas_de_um_aluno(self):
-        r_esperado = [("POO",),('Banco de Dados',),('Raciocinio Logico',)]
         aluna = 'Carla'
+        r_esperado = [('POO',),('Banco de Dados',),('Algoritmos',)]
         self.assertEqual(turmas_de_um_aluno(self.mock_db_config.get('bd'),aluna),r_esperado)
+        aluna2 = 'Giovana'
+        r_esperado2 = []
+        self.assertEqual(turmas_de_um_aluno(self.mock_db_config.get('bd'),aluna2),r_esperado2)
+        aluna3 = 'Julia'
+        r_esperado3 = []
+        self.assertEqual(turmas_de_um_aluno(self.mock_db_config.get('bd'),aluna3),r_esperado3)
+        
         
     def test_alunos_de_uma_turma(self):
+        turma = 'TAD0203'
         r_esperado = [('Carla',),('Lara',),('Liryel',),('Danilo',),('Fernanda',)]
-        self.assertEqual(alunos_de_uma_turma(self.mock_db_config.get('bd')),r_esperado)
+        self.assertEqual(alunos_de_uma_turma(self.mock_db_config.get('bd'),turma),r_esperado)
+        turma2 = 'Racicionio Logico'
+        r_esperado2 = []
+        self.assertEqual(alunos_de_uma_turma(self.mock_db_config.get('bd'),turma2),r_esperado2)
+        turma3 = 'Autoria Web'
+        r_esperado3 = []
+        self.assertEqual(alunos_de_uma_turma(self.mock_db_config.get('bd'),turma3),r_esperado3)
+    
+    def test_quantidade_de_alunos(self):
+        turma = 'POO'
+        r_esperado = [(5,)]
+        self.assertEqual(quantidade_de_alunos(self.mock_db_config.get('bd'),turma),r_esperado)
